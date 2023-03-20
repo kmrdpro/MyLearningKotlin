29d344c5f37e399b45ddb6f591a4cd2d4cdca4f6
Oleg Tikhonov (work) <olegtikhonov1987@gmail.com>
2023-03-20 21:12:38 +0300

    Nichego ne rabotaet

--- app/src/main/java/com/kmrd/myapplication/utils/ImagePicker.kt
+++ app/src/main/java/com/kmrd/myapplication/utils/ImagePicker.kt
@@ -1,19 +1,16 @@
 package com.kmrd.myapplication.utils
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
-import com.kmrd.myapplication.R
+import com.kmrd.myapplication.MainActivity
 import com.kmrd.myapplication.act.EditAdsAct
-import com.kmrd.myapplication.dialogs.RcViewDialogSpinnerAdapter
-import io.ak1.pix.helpers.PixBus
-import io.ak1.pix.helpers.PixEventCallback
-import io.ak1.pix.helpers.addPixToActivity
 import io.ak1.pix.helpers.pixFragment
 import io.ak1.pix.models.Flash
 import io.ak1.pix.models.Mode
 import io.ak1.pix.models.Options
 import io.ak1.pix.models.Ratio
+
 
 object ImagePicker {
     const val REQUEST_CODE_GET_IMAGES = 999
@@ -21,17 +18,20 @@ object ImagePicker {
     fun getImages(context: AppCompatActivity) {
         val options = Options().apply{
             ratio = Ratio.RATIO_AUTO                                    //Image/video capture ratio
-            count = 1                                                   //Number of images to restrict selection count
+            count = 3                                                   //Number of images to restrict selection count
             spanCount = 4                                               //Number for columns in grid
             path = "Pix/Camera"                                         //Custom Path For media Storage
             isFrontFacing = false                                       //Front Facing camera on start
             //videoDurationLimitInSeconds = 10                            //Duration for video recording
-            mode = Mode.Picture                                             //Option to select only pictures or videos or both
-            flash = Flash.Auto                                          //Option to select flash type
+            mode = Mode.Picture                                            //Option to select only pictures or videos or both
+            flash = Flash.Auto                                         //Option to select flash type
             preSelectedUrls = ArrayList<Uri>()                          //Pre selected Image Urls
         }
 
         val pixFragment = pixFragment(options)
+        
+
+
 
 
     }
