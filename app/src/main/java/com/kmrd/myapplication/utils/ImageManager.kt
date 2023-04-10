57b13ddf09873922f54ce4ed2c0ab238fcaa4221
Oleg Tikhonov (Home) <olegtikhonov1987@gmail.com>
2023-04-10 23:51:24 +0300

    Vse kak to opasno

--- app/src/main/java/com/kmrd/myapplication/utils/ImageManager.kt
+++ app/src/main/java/com/kmrd/myapplication/utils/ImageManager.kt
@@ -4,11 +4,17 @@ import android.app.Activity
 import android.graphics.BitmapFactory
 import android.media.ExifInterface
 import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import com.kmrd.myapplication.act.EditAdsAct
 import java.io.File
 import java.io.InputStream
 
 
 object ImageManager {
+    const val MAX_IMAGE_SIZE = 1000
+    const val WIDTH = 0
+    const val HEIGHT = 1
+
     fun getImageSize(uri: Uri, act: Activity): List<Int> {
         val inStream = act.contentResolver.openInputStream(uri)
         val fTemp = File(act.cacheDir, "temp.tmp")
@@ -40,6 +46,30 @@ object ImageManager {
         this.outputStream().use {
             out -> inStream.copyTo(out)
         }
+    }
+
+    fun imageResize(uris: List<Uri>, act: Activity) {
+        val tempList = ArrayList<List<Int>>()
+        for (n in uris.indices) {
+            val size = getImageSize(uris[n], act)
+            val imageRatio = size[WIDTH].toFloat() / size[HEIGHT].toFloat()
+            if (imageRatio > 1) {
+                if (size[WIDTH] > MAX_IMAGE_SIZE) {
+                    tempList.add(listOf(MAX_IMAGE_SIZE, (MAX_IMAGE_SIZE/imageRatio).toInt()))
+                } else {
+                    tempList.add(listOf(size[WIDTH], size[HEIGHT]))
+                }
+            } else {
+                if (size[WIDTH] > MAX_IMAGE_SIZE) {
+                    tempList.add(listOf((MAX_IMAGE_SIZE*imageRatio).toInt(), MAX_IMAGE_SIZE))
+                } else {
+                    tempList.add(listOf(size[WIDTH], size[HEIGHT]))
+                }
+            }
+
+
+        }
+
     }
 
 
